bc3e49e3c44dfd561b4157a7c4f45ea963457d43
dennislundgren <91125819+dennislundgren@users.noreply.github.com>
2021-11-04 22:50:21 +0100

    local storage functionality

--- js/main.js
+++ js/main.js
@@ -1,39 +1,147 @@
-let container = document.createElement("div");
-let input = document.createElement("input");
-let button = document.createElement("button");
-container.classList.add("container", "add");
-input.setAttribute("placeholder", "To do...");
-button.setAttribute("onclick", "add()");
-button.innerHTML = "Press me";
-container.appendChild(input);
-container.appendChild(button);
+let addContainer = document.createElement("div");
+let addInput = document.createElement("input");
+let addButton = document.createElement("button");
 
-document.body.appendChild(container);
+addContainer.classList.add("container", "add");
+addInput.setAttribute("placeholder", "To do...");
+addButton.setAttribute("onclick", "add()");
+addButton.innerHTML = "<i class='fas fa-plus'></i>";
+addContainer.appendChild(addInput);
+addContainer.appendChild(addButton);
 
-let containers = [container];
-let len = containers.length;
+document.body.appendChild(addContainer);
+
+let uniqueId;
+setInterval(() => {
+  uniqueId = Date.now();
+}, 1);
+
+let todoContainer = document.createElement("div");
+let todoInput = document.createElement("input");
+let removeTodoButton = document.createElement("button");
+let doneTodoButton = document.createElement("button");
+
+todoContainer.classList.add("container", "todo");
+todoInput.removeAttribute("placeholder");
+removeTodoButton.setAttribute("onclick", "remove(this)");
+removeTodoButton.innerHTML = "<i class='fas fa-minus'></i>";
+doneTodoButton.setAttribute("onclick", "done()");
+doneTodoButton.innerHTML = "<i class='fas fa-check'></i>";
+todoContainer.appendChild(todoInput);
+todoContainer.appendChild(doneTodoButton);
+todoContainer.appendChild(removeTodoButton);
+
+let idArray = [];
+
+for (let j = 0; j < localStorage.length; j++) {
+  idArray.push(localStorage.key(j));
+  // let todoId = localStorage.key(j);
+  // console.log(todoId);
+  // let todoValue = localStorage.getItem(todoId);
+  // todoInput.setAttribute("value", todoValue);
+  // let todoClone = todoContainer.cloneNode(true);
+  // document.body.appendChild(todoClone);
+}
+
+idArray.sort();
+
+for (let i = 0; i < idArray.length; i++) {
+  let todoId = idArray[i];
+  console.log(todoId);
+  let todoValue = localStorage.getItem(todoId);
+  todoInput.setAttribute("value", todoValue);
+  removeTodoButton.setAttribute("id", todoId);
+  let todoClone = todoContainer.cloneNode(true);
+  document.body.appendChild(todoClone);
+}
+
+window.addEventListener("keyup", function (e) {
+  if (e.key === "Enter") {
+    e.preventDefault();
+
+    add();
+  }
+});
 
 function add() {
-  let value = input.value;
-  containers.push(container.cloneNode(true));
-  document.body.appendChild(containers[len]);
-  let b = containers[len].querySelector("button");
-  let n = containers[len].querySelector("input");
-  b.setAttribute("onclick", "remove(this)");
-  b.innerHTML = "Remove";
-  n.removeAttribute("placeholder");
-  n.setAttribute("value", value);
-  n.style.border = "none";
-  containers[len].classList.remove("add");
-  containers[len].classList.add("todo");
-  len++;
-  input.value = null;
+  localStorage.setItem(uniqueId, addInput.value);
+  addInput.value = "";
+  let todoId = uniqueId;
+  let todoValue = localStorage.getItem(todoId);
+  todoInput.setAttribute("value", todoValue);
+  removeTodoButton.setAttribute("id", todoId);
+  let todoClone = todoContainer.cloneNode(true);
+  document.body.appendChild(todoClone);
 }
 
 function remove(e) {
+  localStorage.removeItem(e.id);
   e.parentNode.remove();
-  len--;
 }
+
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+
+// function add() {
+//   containers.push(container.cloneNode(true));
+//   let value = input.value;
+//   values.push(value);
+//   let b = containers[len].querySelector("button");
+//   let n = containers[len].querySelector("input");
+//   b.setAttribute("onclick", "remove(this)");
+//   b.innerHTML = "<i class='fas fa-minus'></i>";
+//   n.removeAttribute("placeholder");
+//   n.setAttribute("value", value);
+//   n.style.border = "none";
+//   containers[len].classList.remove("add");
+//   containers[len].classList.add("todo");
+//   document.body.appendChild(containers[len]);
+//   input.value = null;
+
+//   for (let i = 0; i < containers.length; i++) {
+//     console.log(containers[i]);
+//   }
+// }
 
 // for (let i = 1; i < 5; i++) {
 //   containers.push(container.cloneNode(true));
